19e697528c6d93f1e5f9e5e8793bdace69a6c5fe
IRCdDev <rooferdappy@gmail.com>
2014-05-21 11:07:32 +0100

    syntaxircd: updated

--- extras/services/atheme/syntaxircd.c
+++ extras/services/atheme/syntaxircd.c
@@ -14,7 +14,7 @@
 #include "pmodule.h"
 #include "protocol/shadowircd.h"
 
-DECLARE_MODULE_V1("protocol/syntaxircd", true, _modinit, NULL, PACKAGE_STRING, "Syntaxnode <support@syntaxnode.co.uk>");
+DECLARE_MODULE_V1("protocol/syntaxircd", true, _modinit, NULL, PACKAGE_STRING, "SyntaxIRCd <root@foxatomic.net>");
 
 /* *INDENT-OFF* */
 
