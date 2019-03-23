1d2821baded598513341384ca6fecb2cdfffe6c3
thecodezero <contact@hyphovy.net>
2019-03-23 13:49:11 -0400

    Fix strip_unprintable on high bit bytes

--- include/inline/stringops.h
+++ include/inline/stringops.h
@@ -30,9 +30,9 @@
 static inline char *
 strip_colour(char *string)
 {
-	char *c = string;
-	char *c2 = string;
-	char *last_non_space = NULL;
+	unsigned char *c = (unsigned char *)string;
+	unsigned char *c2 = (unsigned char *)string;
+	unsigned char *last_non_space = NULL;
 
 	/* c is source, c2 is target */
 	for(; c && *c; c++)
