1e1763a621162fae8a05b57d4fb316ccc30b8585
thecodezero <contact@hyphovy.net>
2019-03-28 08:18:18 -0400

    maybe fixed.

--- modules/m_cmessage.c
+++ modules/m_cmessage.c
@@ -127,8 +127,9 @@ m_cmessage(int p_or_n, const char *command,
 		return 0;
 	}
 
-	if(MyClient(target_p) && (IsSetCallerId(target_p) || (IsSetStaffOnlyMsg(target_p) && !IsOper(source_p)) || (IsSetSslOnlyMsg(target_p) && !IsSSLClient(source_p)) || (IsSetRegOnlyMsg(target_p) && !source_p->user->suser[0])) &&
-	   !accept_message(source_p, target_p) && !IsOper(source_p))
+	if(MyClient(target_p) && (IsSetCallerId(target_p) || (IsSetStaffOnlyMsg(target_p) && !IsOper(source_p)) || (IsSetRegOnlyMsg(target_p) && !source_p->user->suser[0])) &&
+	   !accept_message(source_p, target_p) && !IsOper(source_p)
+           (IsSetSslOnlyMsg(target_p) && !IsSSLClient(source_p)))
 	{
 		if (IsSetRegOnlyMsg(target_p) && !source_p->user->suser[0])
 		{
